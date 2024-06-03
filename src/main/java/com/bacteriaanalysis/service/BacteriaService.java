72c99966e3f229f71502d28bb12ae582b967d7cf
rajug <rajugriet483@gmail.com>
2024-06-03 09:33:03 -0700

    latest code

--- src/main/java/com/bacteriaanalysis/service/BacteriaService.java
+++ src/main/java/com/bacteriaanalysis/service/BacteriaService.java
@@ -36,7 +36,6 @@ public class BacteriaService {
         Map<String, BacteriaSample> bacteriaSampleMap = new HashMap<>();
         Map<String, BacteriaValue> bacteriaValueMap = new HashMap<>();
 
-
         try (ByteArrayInputStream bais = new ByteArrayInputStream(zipData);
              ZipInputStream zis = new ZipInputStream(bais)) {
             ZipEntry zipEntry;
@@ -44,36 +43,33 @@ public class BacteriaService {
                 if (zipEntry.getName().endsWith("cnt.csv")) {
                     // Read the content of the CSV file
                     BufferedReader reader = new BufferedReader(new InputStreamReader(zis));
-                    String line;
-                    reader.readLine();//skipping the first line
-                    // Read each line of the CSV file-
-                    while ((line = reader.readLine()) != null) {
-                        BacteriaSample bacteriaSample = this.getBacteriaSample(line);
+
+                    String tableHeader = reader.readLine();//skipping the first row
+// Read each row of the CSV file-
+                    System.out.println(tableHeader);
+                    String row;
+                    while ((row = reader.readLine())!= null) {
+                        BacteriaSample bacteriaSample = this.getBacteriaSample(tableHeader,row);
                         bacteriaSampleMap.put(bacteriaSample.getBacteriaName(), bacteriaSample);
-
-                        // Do something with each line of the CSV file
+                        // Do something with each row of the CSV file
                     }
-
                 } else if (zipEntry.getName().endsWith("value.csv")) {
                     // Read the content of the CSV file
                     BufferedReader reader = new BufferedReader(new InputStreamReader(zis));
-                    String line;
-                    reader.readLine();
-                    // Read each line of the CSV file
-                    while ((line = reader.readLine()) != null) {
-                        // Do something with each line of the CSV file
-//
-                        BacteriaValue bacteriaSample2 = this.getBacteriaValue(line);
+                    String row1;
+                    String tableHeader = reader.readLine();
+                    // Read each row1 of the CSV file
+                    while ((row1 = reader.readLine()) != null) {
+                        // Do something with each row1 of the CSV file
+                        BacteriaValue bacteriaSample2 = this.getBacteriaValue(tableHeader,row1);
                         bacteriaValueMap.put(bacteriaSample2.getBacteriaName(), bacteriaSample2);
-
                     }
-
                 }
-
             }
         } catch (IOException e) {
             System.out.println("Error occured while");
         }
+
         List<DimSample> dimSampleList = new ArrayList<>();
         for (String bacteriaName : bacteriaValueMap.keySet()) {
             DimSample dimsample1 = new DimSample();
@@ -84,24 +80,22 @@ public class BacteriaService {
         }
         dimSampleList = bacteriaDimSampleDao.saveAll(dimSampleList);
 
+        List<FactSample> factSampleList = saveFactSampleList(bacteriaSampleMap, bacteriaValueMap, dimSampleList);
+        return new ResponseEntity<>(factSampleList, HttpStatus.ACCEPTED);
+    }
+    private List<FactSample> saveFactSampleList(Map<String, BacteriaSample> bacteriaSampleMap,
+                                                Map<String, BacteriaValue> bacteriaValueMap, List<DimSample> dimSampleList) {
         List<FactSample> factSampleList = new ArrayList<>();
-
-
-        for (DimSample newDimSampleObject : dimSampleList) {// For each bacteara-10 bactearia
-
+        for (DimSample newDimSampleObject : dimSampleList) { // For each bacteara-10 bactearia
             String bacteriaName = newDimSampleObject.getBacteriaString();
             BacteriaSample bacteriaSample = bacteriaSampleMap.get(bacteriaName);
             //bacteariaCounts
             Map<String, Integer> bacteriaSampleCounts = bacteriaSample.getPropertiesMap();//10- sample counts
-
             BacteriaValue bacteriaValue = bacteriaValueMap.get(bacteriaName);
             //bacteariaValues
             Map<String, Double> bacteriaSampleValues = bacteriaValue.getPropertiesMap();//10-//10- sample values<Sample Id, value>
-
             for (String sampleId : bacteriaSampleCounts.keySet()) {
-
                 FactSample factSample1 = new FactSample();
-
                 factSample1.setBacteriaFkId(newDimSampleObject.getBacteriaId());//1
                 factSample1.setBacteriaCount(bacteriaSampleCounts.get(sampleId));
                 factSample1.setBacteriaValue(bacteriaSampleValues.get(sampleId));
@@ -111,47 +105,42 @@ public class BacteriaService {
                 factSampleList.add(factSample1);
             }
         }
-       bacteriaFactSampleDao.saveAll(factSampleList);
-
-
-
-        return new ResponseEntity<>(factSampleList, HttpStatus.ACCEPTED);
+        bacteriaFactSampleDao.saveAll(factSampleList);
+        return factSampleList;
     }
 
-    public BacteriaSample getBacteriaSample(String line) {
-        String[] tokens = line.split(",");
+    public BacteriaSample getBacteriaSample(String tableHeader,String dataRow) {
+        String[] dataTokens = dataRow.split(",");
+        String [] headerTokens=tableHeader.split(",");
         BacteriaSample bacteriaSample = new BacteriaSample();
-        bacteriaSample.setBacteriaName(tokens[0]);
+        bacteriaSample.setBacteriaName(dataTokens[0]);
         Map<String, Integer> propertiesMap = new HashMap<>();
-        propertiesMap.put("4AHXT4", Integer.parseInt(tokens[1]));
-        propertiesMap.put("VQBLOB", Integer.parseInt(tokens[2]));
-        propertiesMap.put("QWE1YH", Integer.parseInt(tokens[3]));
-        propertiesMap.put("M32852", Integer.parseInt(tokens[4]));
-        propertiesMap.put("N1GPT5", Integer.parseInt(tokens[5]));
-        propertiesMap.put("9GW59W", Integer.parseInt(tokens[6]));
-        propertiesMap.put("81S8L1", Integer.parseInt(tokens[7]));
-        propertiesMap.put("DURD3C", Integer.parseInt(tokens[8]));
-        propertiesMap.put("XG8W8D", Integer.parseInt(tokens[9]));
-        propertiesMap.put("HD882N", Integer.parseInt(tokens[10]));
+        if(dataTokens.length!=headerTokens.length){
+            return null;
+        }
+        int n=dataTokens.length;
+        for (int i = 1; i < Math.min(headerTokens.length, dataTokens.length); i++) {
+                // Attempt to parse the string into an integer
+                int number = Integer.parseInt(dataTokens[i]);
+                // Put the parsed number into the properties map with the corresponding header token
+                propertiesMap.put(headerTokens[i], number);
+        }
         bacteriaSample.setPropertiesMap(propertiesMap);
         return bacteriaSample;
     }
-
-    public BacteriaValue getBacteriaValue(String line) {
+    public BacteriaValue getBacteriaValue(String tableHeader,String line) {
         String[] tokens = line.split(",");
+        String [] headerToken=tableHeader.split(",");
         BacteriaValue bacteriaValue = new BacteriaValue();
         bacteriaValue.setBacteriaName(tokens[0]);
         Map<String, Double> propertiesMap = new HashMap<>();
-        propertiesMap.put("4AHXT4", Double.parseDouble(tokens[1]));
-        propertiesMap.put("VQBLOB", Double.parseDouble(tokens[2]));
-        propertiesMap.put("QWE1YH", Double.parseDouble(tokens[3]));
-        propertiesMap.put("M32852", Double.parseDouble(tokens[4]));
-        propertiesMap.put("N1GPT5", Double.parseDouble(tokens[5]));
-        propertiesMap.put("9GW59W", Double.parseDouble(tokens[6]));
-        propertiesMap.put("81S8L1", Double.parseDouble(tokens[7]));
-        propertiesMap.put("DURD3C", Double.parseDouble(tokens[8]));
-        propertiesMap.put("XG8W8D", Double.parseDouble(tokens[9]));
-        propertiesMap.put("HD882N", Double.parseDouble(tokens[10]));
+        if(tokens.length!=headerToken.length){
+            return null;
+        }
+        int n=tokens.length;
+        for(int i=1;i<n;i++){
+            propertiesMap.put(headerToken[i], Double.parseDouble(tokens[i]));
+        }
         bacteriaValue.setPropertiesMap(propertiesMap);
         return bacteriaValue;
     }
